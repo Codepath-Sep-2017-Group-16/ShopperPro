f06bb433696ec282cb1da51d0171040d3d746fa1
rndeshpande <rahulnd@gmail.com>
2017-10-13 02:49:20 -0700

    DatabaseUtils updated with base User and List paths

--- app/src/main/java/com/codepath/socialshopper/socialshopper/Utils/DatabaseUtils.java
+++ app/src/main/java/com/codepath/socialshopper/socialshopper/Utils/DatabaseUtils.java
@@ -1,28 +1,35 @@
 package com.codepath.socialshopper.socialshopper.Utils;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by rdeshpan on 10/13/2017.
  */
 
 public class DatabaseUtils {
 
-    public void getActiveLists() {
+    static final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    private static final String baseUser = "users/";
+    private static final String baseLists = "lists/";
+
+    public static void getActiveLists() {
         // TODO : implement this
     }
 
-    public void getPastLists() {
+    public static void getPastLists() {
         // TODO : implement this
     }
 
-    public void saveList() {
+    public static void saveList() {
         // TODO : implement this
     }
 
-    public void deleteList() {
+    public static void deleteList() {
         // TODO : implement this
     }
 
-    public void acceptList() {
+    public static void acceptList() {
         // TODO : implement this
     }
 }
