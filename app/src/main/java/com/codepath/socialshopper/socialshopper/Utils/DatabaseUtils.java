34346467c50ec9d6b4f60aa1c16cfc8fadcc236b
rndeshpande <rahulnd@gmail.com>
2017-10-13 01:48:16 -0700

    Firebase Real-Time Database Integration
    1. Firebase Real-Time database added to the project
    2. DatabaseUtils.java :  interface for  accessing database related functions

---
+++ app/src/main/java/com/codepath/socialshopper/socialshopper/Utils/DatabaseUtils.java
@@ -0,0 +1,28 @@
+package com.codepath.socialshopper.socialshopper.Utils;
+
+/**
+ * Created by rdeshpan on 10/13/2017.
+ */
+
+public class DatabaseUtils {
+
+    public void getActiveLists() {
+        // TODO : implement this
+    }
+
+    public void getPastLists() {
+        // TODO : implement this
+    }
+
+    public void saveList() {
+        // TODO : implement this
+    }
+
+    public void deleteList() {
+        // TODO : implement this
+    }
+
+    public void acceptList() {
+        // TODO : implement this
+    }
+}
