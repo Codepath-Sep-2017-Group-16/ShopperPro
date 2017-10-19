87c83b821c2789e95b8a3116a01bc729adf907e0
Sriram Santosh Aripirala <sriramsantosh_aripirala@intuit.com>
2017-10-19 00:32:26 -0700

    Shifted the activities around a bit

--- app/src/main/java/com/codepath/socialshopper/socialshopper/Activities/LoginActivity.java
+++ app/src/main/java/com/codepath/socialshopper/socialshopper/Activities/LoginActivity.java
@@ -94,7 +94,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     private void showHomeScreen() {
-        Intent intent = new Intent(this, ChooseStoreActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
