74e6616d3095b3e568bf144b96d166a125f9b124
rndeshpande <rahulnd@gmail.com>
2017-10-29 15:25:40 -0700

    Publish List: Added Add/Remove buttons to publish list view

--- app/src/main/java/com/codepath/socialshopper/socialshopper/Adapters/ShoppingListArrayAdapter.java
+++ app/src/main/java/com/codepath/socialshopper/socialshopper/Adapters/ShoppingListArrayAdapter.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.socialshopper.socialshopper.Models.ShoppableItem;
@@ -37,11 +38,58 @@ public class ShoppingListArrayAdapter extends RecyclerView.Adapter<ShoppingListA
 
     @Override
     public void onBindViewHolder(ShoppingListViewHolder holder, int position) {
-        final ShoppableItem shoppableItem = shoppableItems.get(position);
-        holder.tvItemQty.setText(Integer.toString(shoppableItem.getmItemQty()));
-        holder.tvItemName.setText(shoppableItem.getmItemName());
-        holder.tvItemBrand.setText(shoppableItem.getmItemBrand());
-        Log.i(TAG, "onBindViewHolder");
+        ShoppableItem item = shoppableItems.get(position);
+
+        holder.ivItemImage.setImageResource(R.drawable.apple);
+
+        if(item.getmItemIconFileName().contains("banana"))
+            holder.ivItemImage.setImageResource(R.drawable.banana);
+        else if(item.getmItemIconFileName().contains("apple")){
+            holder.ivItemImage.setImageResource(R.drawable.apple);
+        }else if(item.getmItemIconFileName().contains("lemon")){
+            holder.ivItemImage.setImageResource(R.drawable.lemon);
+        }else if(item.getmItemIconFileName().contains("strawberry")){
+            holder.ivItemImage.setImageResource(R.drawable.strawberry);
+        }else if(item.getmItemIconFileName().contains("carrot")){
+            holder.ivItemImage.setImageResource(R.drawable.carrot);
+        }else if(item.getmItemIconFileName().contains("tomato")){
+            holder.ivItemImage.setImageResource(R.drawable.tomato);
+        }else if(item.getmItemIconFileName().contains("almond")){
+            holder.ivItemImage.setImageResource(R.drawable.almond);
+        }else if(item.getmItemIconFileName().contains("beans")){
+            holder.ivItemImage.setImageResource(R.drawable.beans);
+        }else if(item.getmItemIconFileName().contains("butter")){
+            holder.ivItemImage.setImageResource(R.drawable.cauliflower);
+        }else if(item.getmItemIconFileName().contains("cheese")){
+            holder.ivItemImage.setImageResource(R.drawable.cheese);
+        }else if(item.getmItemIconFileName().contains("duck")){
+            holder.ivItemImage.setImageResource(R.drawable.duck);
+        }else if(item.getmItemIconFileName().contains("filet")){
+            holder.ivItemImage.setImageResource(R.drawable.filet);
+        }else if(item.getmItemIconFileName().contains("grapes")){
+            holder.ivItemImage.setImageResource(R.drawable.grapes);
+        }else if(item.getmItemIconFileName().contains("lettuce")){
+            holder.ivItemImage.setImageResource(R.drawable.lettuce);
+        }else if(item.getmItemIconFileName().contains("milk")){
+            holder.ivItemImage.setImageResource(R.drawable.milk);
+        }else if(item.getmItemIconFileName().contains("potato")){
+            holder.ivItemImage.setImageResource(R.drawable.potato);
+        }else if(item.getmItemIconFileName().contains("radish")){
+            holder.ivItemImage.setImageResource(R.drawable.radish);
+        }else if(item.getmItemIconFileName().contains("ribs")){
+            holder.ivItemImage.setImageResource(R.drawable.ribs);
+        }else if(item.getmItemIconFileName().contains("steak")){
+            holder.ivItemImage.setImageResource(R.drawable.steak);
+        }else if(item.getmItemIconFileName().contains("strawberry")){
+            holder.ivItemImage.setImageResource(R.drawable.strawberry);
+        }else if(item.getmItemIconFileName().contains("yoghurt")){
+            holder.ivItemImage.setImageResource(R.drawable.yoghurt);
+        }
+
+        holder.tvItemBrand.setText(item.getmItemBrand());
+        holder.tvItemName.setText(item.getmItemName());
+        holder.tvItemQuantity.setText(Integer.toString(item.getmItemQty()));
+        holder.tvItemMeasure.setText(item.getItemMeasure());
     }
 
     @Override
@@ -50,14 +98,19 @@ public class ShoppingListArrayAdapter extends RecyclerView.Adapter<ShoppingListA
     }
 
     public class ShoppingListViewHolder extends RecyclerView.ViewHolder {
-        TextView tvItemQty;
-        TextView tvItemName;
-        TextView tvItemBrand;
+        private TextView tvItemName;
+        private TextView tvItemBrand;
+        private TextView tvItemQuantity;
+        private TextView tvItemMeasure;
+        private ImageView ivItemImage;
+
         public ShoppingListViewHolder(View itemView) {
             super(itemView);
-            tvItemBrand = (TextView) itemView.findViewById(R.id.tvItemBrandList);
-            tvItemName = (TextView) itemView.findViewById(R.id.tvItemNameList);
-            tvItemQty = (TextView) itemView.findViewById(R.id.tvItemQtyList);
+            tvItemName = (TextView) itemView.findViewById(R.id.tvItemName);
+            tvItemQuantity = (TextView) itemView.findViewById(R.id.tvItemQuantity);
+            tvItemMeasure = (TextView) itemView.findViewById(R.id.tvItemMeasure);
+            tvItemBrand = (TextView) itemView.findViewById(R.id.tvItemBrand);
+            ivItemImage = (ImageView) itemView.findViewById(R.id.ivItemImage);
         }
     }
 }
