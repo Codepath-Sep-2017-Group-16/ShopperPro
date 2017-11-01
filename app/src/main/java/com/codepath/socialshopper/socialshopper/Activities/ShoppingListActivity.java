20dff8f40752f0a1058ebb91aa08e3798888a45e
Greeshma Umapathi <greeshmakashyap22@gmail.com>
2017-10-31 20:18:38 -0700

    cleared shopping list after sub

--- app/src/main/java/com/codepath/socialshopper/socialshopper/Activities/ShoppingListActivity.java
+++ app/src/main/java/com/codepath/socialshopper/socialshopper/Activities/ShoppingListActivity.java
@@ -67,6 +67,7 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         DatabaseUtils.updateListStatus(shoppingList.getListId(), Status.PLACED);
         startActivity(intent);
+        shoppingList.clear();
         finish();
     }
 
